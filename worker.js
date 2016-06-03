fea7669318f36a5c96afd62fbbc35ecb330df017
Sean LeBlanc <sean.s.leblanc@gmail.com>
2016-06-03 00:55:23 -0400

    Delete worker.js

--- worker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener("message", function(event){
-	self.postMessage({cmd:"msg", data:event.data});
-	self.postMessage({cmd:"msg", data:event.data[0].img.width});
-	self.postMessage({cmd:"complete", data:null});
-}, false);
